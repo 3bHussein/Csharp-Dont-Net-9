a88005e176cddc8de4dfda132b865a642caafc5f
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2025-01-08 00:06:10 +0400

    /

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("C# Programmer");
+
+// Console.WriteLine("C# Programmer i'm back to C# i love MicroSoft");
+
+Console.WriteLine(args[0]);
 
 
-
-
+    
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8f1ad4bc6d16f1426b90d65d7fa4e48c95d4b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f40ef6d22d33589c88a6a48cee490cca10e5642e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
