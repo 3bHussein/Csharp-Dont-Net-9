59102b7865b811a4b590acc39e321cb90c90a475
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2025-01-24 00:52:54 +0400

    /

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -87,3 +87,4 @@ name=Console.ReadLine();
 System.Console.WriteLine("hello"+name);
 
 System.Console.WriteLine("hello"+name);
+System.Console.WriteLine("hello"+name);
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef240350b7d0afc619d440ee7168ed4351aa240a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29a504f4003aab9a72809554804a85bcbf85523")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
