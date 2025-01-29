8579d5870e91406118a821b487cd2abeeca07678
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2025-01-29 22:39:45 +0400

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -87,4 +87,5 @@ name=Console.ReadLine();
 System.Console.WriteLine("hello"+name);
 System.Console.WriteLine("hello"+name);
 System.Console.WriteLine("hello"+name);
+System.Console.WriteLine("hello"+name);
 
