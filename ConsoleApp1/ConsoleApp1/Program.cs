5fa364970cf2f52b52af687da07d217340de0212
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2025-05-02 14:26:25 +0400

    add new project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
