d30fa0f5ec034ec0c586230abdc12689a942814e
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2025-07-07 21:08:16 +0400

    /

--- DatingApp/API/Enities/AppUser.cs
+++ DatingApp/API/Enities/AppUser.cs
@@ -13,7 +13,7 @@ public class AppUser
     [Required]
 
     public string? name { get; set; }
-    public string? name { get; set; }
+    public string? Email { get; set; }
 
     
 
--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd39dda5f4542db8c7c7f263abc0e5a998c690e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eafd0e1fd2fd4128cf6073106f02beb8eb693ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
