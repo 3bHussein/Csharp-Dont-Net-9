f02505634d1cb7f1bd2ec4c54d507f9503b55d9e
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2025-07-10 01:25:35 +0400

    Update AppDbContext.cs

--- DatingApp/API/Data/AppDbContext.cs
+++ DatingApp/API/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace API.Data;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     
-    // 
+    
 // public AppDBContext(DbContextOptions options) :base(options)
     // {
 
